662d5ab9429332df3ca4261db60e75bd47041e9c
grego1201 <gregoriopatinoesteo@gmail.com>
2021-05-17 21:53:30 +0200

    Add CRUD fencers view

---
+++ app/controllers/fencers_controller.rb
@@ -0,0 +1,50 @@
+class FencersController < ApplicationController
+  def index
+    @fencers = Fencer.all
+  end
+
+  def show
+    @fencer = Fencer.find(params[:id])
+  end
+
+  def new
+    @fencer = Fencer.new
+  end
+
+  def create
+    @fencer = Fencer.new(safe_params)
+
+    if @fencer.save
+      redirect_to @fencer
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @fencer = Fencer.find(params[:id])
+  end
+
+  def update
+    @fencer = Fencer.find(params[:id])
+
+    if @fencer.update(safe_params)
+      redirect_to @fencer
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @fencer = Fencer.find(params[:id])
+    @fencer.destroy
+
+    redirect_to fencers_path
+  end
+
+  private
+
+  def safe_params
+    params.require(:fencer).permit(:name, :surname, :second_surname, :nationality, :team)
+  end
+end
