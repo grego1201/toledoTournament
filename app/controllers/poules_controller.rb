c790c8c695b17dbe21e899b0175fb20932c7f4e8
grego1201 <gregoriopatinoesteo@gmail.com>
2021-05-25 23:43:19 +0200

    Fix poules generation

--- app/controllers/poules_controller.rb
+++ app/controllers/poules_controller.rb
@@ -10,16 +10,20 @@ class PoulesController < ApplicationController
     team_ids = Team.all.pluck(:id).shuffle
 
     total_teams = team_ids.count
-    if (total_teams % max_fencers_per_poule).zero?
+    extra_fencers = total_teams % max_fencers_per_poule
+    if extra_fencers.zero?
       group_of_ids = team_ids.in_groups_of(max_fencers_per_poule)
     else
-      group_of_ids = team_ids.in_groups_of(max_fencers_per_poule - 1)
-      group_of_ids.last.each_with_index do |team_id, index|
-        next if team_id.nil?
-        group_of_ids[index] = group_of_ids[index] + [team_id]
+      total_groups = (total_teams / max_fencers_per_poule) + 1
+      group_of_ids = []
+
+      total_groups.times do |i|
+        group_of_ids[i] = []
       end
 
-      group_of_ids.pop
+      team_ids.each_with_index do |team_id, index|
+        group_of_ids[index % total_groups] << team_id
+      end
     end
 
     group_of_ids.each do |teams|
