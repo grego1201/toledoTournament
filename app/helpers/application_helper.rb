85ab2bd3402802859bc9f7cf98c089c51586ae6c
grego1201 <gregoriopatinoesteo@gmail.com>
2021-05-30 23:51:57 +0200

    Show groups in index

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def team_name(team_id)
+    team = Team.find(team_id)
+    team.name || team.id
+  end
+
+  def team_fencer_names(team_id)
+    team = Team.find(team_id)
+    names = ""
+    team.fencers.each do |fencer|
+      names += "|| #{fencer.name} #{fencer.surname} - #{fencer.club} "
+    end
+
+    names
+  end
 end
