9ce998da35efbc8967138656008d9761a5eefc4c
grego1201 <gregoriopatinoesteo@gmail.com>
2021-05-30 23:21:30 +0200

    Add tableaus to elimination groups

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_30_185908) do
+ActiveRecord::Schema.define(version: 2021_05_30_210037) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2021_05_30_185908) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.json "tableaus"
   end
 
   create_table "fencers", force: :cascade do |t|
