2f0e936afd1329c9c43f7c32cdfc9b41b7b8e38b
grego1201 <gregoriopatinoesteo@gmail.com>
2021-06-03 20:06:18 +0200

    Add index to poule

--- app/controllers/poules_controller.rb
+++ app/controllers/poules_controller.rb
@@ -158,14 +158,17 @@ class PoulesController < ApplicationController
   end
 
   def calculate_results_from_poule
-    @poule.poule_matches.sort.to_h.map do |key, value|
-      [
-        team_index_name(key),
-        nil,
-        team_match_results(key, value, @poule),
-        nil,
-        team_statistics(key, value, @poule)
-      ].flatten
+    [].tap do |results|
+      @poule.poule_matches.sort.to_h.each_with_index do |value, index|
+        key, value = value
+        results << [
+          team_index_name(key),
+          index + 1,
+          team_match_results(key, value, @poule),
+          nil,
+          team_statistics(key, value, @poule)
+        ].flatten
+      end
     end
   end
 
